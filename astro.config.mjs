606b1ef52aca449ffa420fe2ecaa2fe2a5bdc2ed
Guilherme Diego <guilherme.albino.francisco@gmail.com>
2023-04-13 11:26:35 +0200

    revert to functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify({
+    builders: true
+  })
 });
 
